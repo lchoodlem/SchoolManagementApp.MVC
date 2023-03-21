833ba21859d7d4540100ffa344e610ab28ac1645
Delbert R. Hood <delbert.r.hood@nasa.gov>
2023-03-21 09:50:19 -0500

    FK 1 to many many to many etc End of Class

---
+++ Models/ClassEnrollmentViewModel.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SchoolManagementApp.MVC.Data;
+
+namespace SchoolManagementApp.MVC.Models
+{
+    public class ClassEnrollmentViewModel
+    {
+        public ClassViewModel? Class {get; set;}
+        public List<StudentEnrollmentViewModel> Students {get; set;} = new 
+           List<StudentEnrollmentViewModel>();
+        
+    }
+
+
+}
